91ae3fec9ef00535f065f58f2e2796b715ab210c
Tai <tainancmoraes@gmail.com>
2022-09-05 06:41:50 -0300

    testando

---
+++ src/pages/Manutence.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="surface-card flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
+            <div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, rgba(247, 149, 48, 0.4) 10%, rgba(247, 149, 48, 0) 30%);">
+                <div class="surface-card h-full w-full m-0 py-7 px-4" style="border-radius:53px;">
+                    <div class="grid flex flex-column align-items-center">
+                        <div class="flex justify-content-center align-items-center bg-orange-500 border-circle" style="width:3.2rem; height:3.2rem;">
+                            <i class="pi pi-fw pi-lock text-2xl text-50"></i>
+                        </div>
+                        <h1 class="text-900 font-bold text-4xl lg:text-5xl mb-2">GALAXIA BURGUER</h1>
+                        <span class="text-gray-600 text-center">Site em Manutenção!! <br> Aproveite e visite nosso restaurente!!<br>Iremos te esperar :)</span>
+                        <img src="layout/images/asset-access.svg" alt="Access denied" class="mt-5" width="80%">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
